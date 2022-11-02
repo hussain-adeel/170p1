6f5a300a82554018a51c1a6fc41b33fb9322f510
hussain-adeel <adeel@adeelh.me>
2022-11-01 17:40:25 -0700

    Added Node class logic, repeat states

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import queue
+import time
+import copy
 
 
 default_easy = [[1, 2, 3],[4, 5, 6],[7, 0, 8]]
@@ -52,14 +55,54 @@ def driver():
     print("Select an algorithim to use with your puzzle\n1. Unform Cost Search\n2. A* with Misplaced Tile heuristic.\n3. A* with Manhattan Distance heuristic")
     select3 = int(input("Selection: "))
 
-    solve(puzzle, select3)
+    start_time = time.time()
+    result = general_search(puzzle, select3)
+    end_time = time.time()
+    total_time = round(end_time-start_time, 1)
+    print("This attempt took: " + str(total_time) + " seconds.")
 
-def solve(puzzle, algorithim):
-    if (algorithim == 1):
-        unicost(puzzle)
-    elif (algorithim == 2):
-        misplace(puzzle)
-    elif (algorithim == 3):
-        manhattan(puzzle)
+    if (result == "failed"): print("Could not complete search...")
+    elif (result == "success"): print("REACH...")
+
+def print_puzzle(puzzle):
+    for i in range(0, 3):
+        print(puzzle[i])
+    print('\n')
+
+class Node:
+    def __init__(self, state, hn, gn):
+        self.state = state
+        self.hn = hn
+        self.gn = gn
+        self.fn = hn + gn
+    def getState(self):
+        return self.state
+    def getFn(self):
+        return self.fn
+    def getHn(self):
+        return self.hn
+    def getGn(self):
+        return self.gn
+    
+        # we need: hn, fn, gn
+
+def goal(puzzle):
+    if (puzzle == goal_state): return 1
+    else: return 0
+
+def general_search(puzzle, algorithim):
+    nodes = queue.PriorityQueue();
+    initalState = Node(puzzle, 0, 0)
+    nodes.put(initalState) #inital state
+    print("Starting State:")
+    print_puzzle(initalState.getState())
+    repeatStates = {};
+
+    while(1):
+        if (nodes.empty()): return "failed"
+        currNode = nodes.get()
+        if (goal(currNode.getState()) == 1): return "success"
+        repeatStates += currNode.getState();
+
 
 driver();
