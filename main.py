01b72453050e41b7aeb7a255934826b5adf3006c
Adeel Hussain <adeel@adeelh.me>
2022-11-04 03:51:57 -0400

    Fixed issue where nodes were repeating, UCS

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
-import queue
-import time
-import copy
+import queue # TO ACCESS PRIORITY QUEUE
+import time # TO CALCULATE AMNT OF TIME PROGRAM TAKES
+import copy # TO DEEP COPY LISTS OF LISTS (cannot lose values)
 
 
-default_easy = [[1, 2, 3],[4, 0, 6],[7, 5, 8]]
-default = [[0, 1, 2],[4, 5, 3],[7, 8, 6]]
-default_hard = [[8, 7, 1],[6, 0, 2],[5, 4, 3]]
+# Some puzzles taken from Dr. Keogh's project handout (may be edited to test)
+default_easy = [[1, 3, 6],[5, 0, 7],[4, 8, 2]]
+default = [[1, 6, 7],[5, 0, 3],[4, 8, 2]]
+default_hard = [[0, 7, 2],[4, 6, 1],[3, 5, 8]]
+
+# End-goal of what we want
 goal_state = [[1, 2, 3],[4, 5, 6],[7, 8, 0]]
-nodes_expanded = 0
 
-
+# Main driver class of program, calls all other functions to make program run properly
 def driver():
+
     print("Welcome to the CS 170 Puzzle Solver...")
     print("Please select an option below to continue:")
     print("1. Default Puzzle")
     print("2. Custom Puzzle")
 
+    # This select stores value of if user wants to use a default or custom value
     select = int(input("Selection: "));
 
-    # USER SELECTS PUZZLE OR CREATES CUSTOM PUZZLE
+    # IF-statement that allows user to choose from default values or create own
     if (select == 1):
         print("Please select a difficulty for the default puzzle...")
         print("1. Easy Puzzle")
         print("2. Medium Puzzle")
         print("3. Hard Puzzle")
 
+        #this select stores user's choice of puzzle difficulty
         select2 = int(input("Selection: "))
 
         if (select2 == 1):
@@ -36,6 +41,8 @@ def driver():
     elif (select == 2):
         print("Type numbers with a space between them, use '0' as the blank space...")
         print("You will type numbers per row... hit ENTER after you are done entering for that row")
+        
+        # User enters  puzzle to be used 
 
         row_one = input("Enter the first row: ")
         row_two = input("Enter the second row: ")
@@ -61,10 +68,10 @@ def driver():
     elif (select3 == 2): algorithim = mt
     elif (select3 == 3): algorithim = m
 
-    start_time = time.time()
+    start_time = time.time() # keeps track of start time of the program
     result = general_search(puzzle, algorithim)
-    end_time = time.time()
-    total_time = round(end_time-start_time, 1)
+    end_time = time.time() # same as start, tracks end time
+    total_time = round(end_time-start_time, 1) # end - start = total time elapsed
     print("This attempt took: " + str(total_time) + " seconds.")
 
     if (result == "failed"): print("Could not complete search...")
@@ -72,11 +79,13 @@ def driver():
         print("Stopping Program...")
 
 
+# Simple funciton to print puzzle (taken from DK's handout)
 def print_puzzle(puzzle):
     for i in range(0, 3):
         print(puzzle[i])
     print('\n')
 
+# Simple node class to store basic info of 8 puzzle nodes (the puzzle, cost, depth, fn, zero-index)
 class Node:
     def __init__(self, state, hn, gn):
         self.state = state
@@ -95,115 +104,133 @@ class Node:
         for i, k in enumerate(self.state):
             for l, m in enumerate(k):
                 if m == 0: return [l, i]
+    # this is used by the priority queue to determine how to order these nodes
     def __lt__(self, compare):
         return self.fn < compare.getFn()
 
-
+# simple function to see if we have reaeched the goal state 
 def goal(puzzle):
     if (puzzle == goal_state): return 1
     else: return 0
 
-def ucs(nodes, expanded):
-    nodes_copy = nodes;
+# function to sort PQ using Uniform Cost Search (notice we have cost set to 0 for all...
+# ... operations so this is basically bfs )
+def ucs(nodes, moves):
+    new_nodes = nodes;
 
-    for n in expanded:
-        nodes_copy.put(n)
+    for node in moves:
+        new_nodes.put(node)
 
-    return nodes_copy
+    return new_nodes
 
+# functin to sort PQ using A* w/ the Missing Tile Heuristic
 def mt(nodes, moves):
     return
+
+#function to sort PQ using A8 w/ the Manhattan Heuristic
 def m(nodes, moves):
     return
 
-numNodesExpanded = 0
-
 def expand(currNode, repeatStates):
-    # NEED INDEX OF 0 STATE
 
-    zeroXY = currNode.getZeroIndex()
-    zeroX = zeroXY[0]; 
-    zeroY = zeroXY[1];
-    moves = list();
-    global numNodesExpanded
+    zeroXY = currNode.getZeroIndex() # get 0-index (where there blank space is)
 
-    # CHECK UP
+    # Need to decompose to their own variables 
+    zeroX = zeroXY[0]
+    zeroY = zeroXY[1]
+
+    # list to store moves being added to PQ after this expansion
+    moves = []
+
+    # Check if blank is allowed to move up (i.e. not against the top edge), 
+    # then deepcopy and check for repeat state, if all is good then add to move list
     if (zeroY != 0):
         up_copy = copy.deepcopy(currNode.getState())
         up_copy[zeroY][zeroX] = up_copy[zeroY - 1][zeroX]
         up_copy[zeroY - 1][zeroX] = 0
         if repeatStates.get(tuple(tuple(s) for s in up_copy)) != 'R':
-            #print("append up")
             up_node = Node(up_copy,  currNode.getHn(), currNode.getGn() + 1);
             moves.append(up_node);
-            numNodesExpanded+=1;
 
-    # CHECK DOWN
+    # Same concept as up except for down (not against bottom edge...)
     if (zeroY != 2):
         down_copy = copy.deepcopy(currNode.getState())
         down_copy[zeroY][zeroX] = down_copy[zeroY + 1][zeroX]
         down_copy[zeroY + 1][zeroX] = 0
         if repeatStates.get(tuple(tuple(s) for s in down_copy)) != 'R':
-            #print("append down")
             down_node = Node(down_copy,  currNode.getHn(), currNode.getGn() + 1);
             moves.append(down_node);
-            numNodesExpanded+=1;
 
-    # CHECK LEFT
+    # Same concept as up except for left (not against left edge...)
     if (zeroX != 0):
         left_copy = copy.deepcopy(currNode.getState())
         left_copy[zeroY][zeroX] = left_copy[zeroY][zeroX - 1]
         left_copy[zeroY][zeroX - 1] = 0
-        
         if repeatStates.get(tuple(tuple(s) for s in left_copy)) != 'R':
-            #print("append left")
             left_node = Node(left_copy,  currNode.getHn(), currNode.getGn() + 1);
             moves.append(left_node);
-            numNodesExpanded+=1;
 
-    # CHECK RIGHT
+    # Same concept as up except for right (not against right edge...)
     if (zeroX != 2):
         right_copy = copy.deepcopy(currNode.getState())
         right_copy[zeroY][zeroX] = right_copy[zeroY][zeroX + 1]
         right_copy[zeroY][zeroX + 1] = 0
         if repeatStates.get(tuple(tuple(s) for s in right_copy)) != 'R':
-            #print("append right");
             right_node = Node(right_copy, currNode.getHn(), currNode.getGn() + 1);
             moves.append(right_node);
-            numNodesExpanded+=1;
-
-
-
-    #for move in moves:
-    #    print_puzzle(move.getState());
-
+    
     return moves
 
 def general_search(puzzle, algorithim):
-    global numNodesExpanded
-    nodes = queue.PriorityQueue();
+
+    # Create Priority Queue, declare initalize needed variables
+    nodes = queue.PriorityQueue()
+    repeatStates = {} # using a python dictonary
+    maxQueueSize = 1
+    numNodesExpanded = 0
+
+    # Add inital problem state as first node
     initalState = Node(puzzle, 0, 0)
-    nodes.put(initalState) #inital state
+    nodes.put(initalState)
     print("Starting State:")
     print_puzzle(initalState.getState())
-    repeatStates = {};
-    maxQueueSize = 1;
-
+    
+    # Ensures no repeat of the inital state by adding to dict (need to convert to tuple) 
+    # and having index be the puzzle already found
+    stateTuple = tuple(tuple(s) for s in initalState.getState())
+    repeatStates[stateTuple] = "R"
+    
     while(1):
+        # no solution and no nodes left, search failed...
         if (nodes.empty()): return "failed"
+
+        # pop current node off PQ
         currNode = nodes.get()
+
+         # check if goal state reached
         if (goal(currNode.getState()) == 1):
             print("REACHED GOAL STATE!")
             print("The maximum queue size was: " + str(maxQueueSize))
             print("The depth was: " + str(currNode.getGn()))
             print("The number of nodes expanded was: " + str(numNodesExpanded))
             return "success"
-        stateTuple = tuple(tuple(s) for s in currNode.getState())
-        repeatStates[stateTuple] = "R"
-        print(stateTuple)
+        
+        # If not goal, then we need to expand it
         print("The best state to expand with a g(n) = " + str(currNode.getGn()) + " and h(n) = " + str(currNode.getHn()) + " is...")
-        nodes = algorithim(nodes, expand(currNode, repeatStates))
-        print_puzzle(currNode.getState());
-        if (nodes.qsize() > maxQueueSize): maxQueueSize = nodes.qsize();
+        print_puzzle(currNode.getState())
 
-driver();
+        # Need to sure no repeats of moves we already found through expanding
+        new_moves = expand(currNode, repeatStates)
+        for move in new_moves:
+            t = tuple(tuple(x) for x in move.getState())
+            repeatStates[t] = 'R'
+
+        # Use queueing function selected to order PQ to better come up with solution
+        nodes = algorithim(nodes, new_moves)
+        numNodesExpanded+=1
+
+        # Checking if max q size has been overtaken 
+        if (nodes.qsize() > maxQueueSize): 
+            maxQueueSize = nodes.qsize()
+
+driver()
